d01c7228cd5acc649342a11c4de865e5439d397e
genki utsumi <zabanya0770@gmail.com>
2023-07-14 12:01:29 +0900

    fix: mapをメインページに変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
       get 'map'
     end
   end
-  root 'posts#index'
+  root 'posts#map'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
