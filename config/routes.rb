6c5f6d49da6c36d577340f0bdba72b8aec32ca1f
genki utsumi <zabanya0770@gmail.com>
2023-07-17 00:21:14 +0900

    add: マイページ作成用のUsersコントローラを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :users, only:[:show]
+  resources :favorites, only:[:create, :destroy]
   resources :posts do
     collection do
       get 'map'
     end
   end
-  resources :favorites, only:[:create, :destroy]
+  
   root 'posts#map'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
