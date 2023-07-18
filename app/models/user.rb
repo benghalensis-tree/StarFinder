df4390efb4a9a5beb6f4f3f0f465303110abc07e
genki utsumi <zabanya0770@gmail.com>
2023-07-18 20:32:39 +0900

    add: 外部情報を元にアカウントを作成するコードを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,30 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
   has_many :posts, dependent: :destroy
   has_many :favorites
   has_many :favorite_posts, through: :favorites, source: :post
   has_many :comments, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(google)
+
   def self.create_unique_string
     SecureRandom.uuid
+  end
+  
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+  
+    unless user
+      user = User.new(email: auth.info.email,
+                      provider: auth.provider,
+                      uid:      auth.uid,
+                      password: Devise.friendly_token[0, 20],
+                                   )
+    end
+    user.save
+    user
   end
 end
