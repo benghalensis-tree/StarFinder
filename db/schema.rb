d2a97d1564e50274576baccbe46cf635e2b9d349
genki utsumi <zabanya0770@gmail.com>
2023-07-19 17:39:43 +0900

    add: お気に入りの数でソートするためにpostsにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_19_041456) do
+ActiveRecord::Schema.define(version: 2023_07_19_083614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2023_07_19_041456) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "favorite_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
