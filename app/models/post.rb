951dd669cd93ae6525cba670d58b36d9c8c91d16
genki utsumi <zabanya0770@gmail.com>
2023-07-19 15:33:42 +0900

    add: 住所で検索機能を実装
    下記エラーが発生した。提示されたコードをpostモデルに記述した事で動作した。
    Define a `ransackable_associations` class method in your `Post`

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,5 +14,9 @@ class Post < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["access_date", "address", "content", "created_at", "id", "image", "latitude", "longitude", "title", "updated_at", "user_id", "view_count"]
   end
+  
+  def self.ransackable_associations(auth_object = nil)
+    ["comments", "favorite_users", "favorites", "user", "view_counts"]
+  end
 
 end
