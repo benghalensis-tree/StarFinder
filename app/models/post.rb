afef879b9625540e9f2101b5f4ad932a96d12b41
genki utsumi <zabanya0770@gmail.com>
2023-07-19 19:36:37 +0900

    fix: 投稿を削除したときに閲覧数やおに入りのレコードも一緒に削除するように修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,10 @@ class Post < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: lambda {|obj| obj.address_changed?}
   belongs_to :user
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   has_many :comments, dependent: :destroy
-  has_many :view_counts
+  has_many :view_counts, dependent: :destroy
 
   def self.ransackable_attributes(auth_object = nil)
     ["access_date", "address", "content", "created_at", "id", "image", "latitude", "longitude", "title", "updated_at", "user_id", "view_count", "favorite_count"]
