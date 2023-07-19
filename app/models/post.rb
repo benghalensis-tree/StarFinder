fbbbd4e7be9d600da81ca68e39c55ec3558abf9b
genki utsumi <zabanya0770@gmail.com>
2023-07-19 13:32:57 +0900

    add: userとpostをview_countsに関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,6 @@ class Post < ApplicationRecord
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: :user
   has_many :comments, dependent: :destroy
+  has_many :view_counts
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :favorite_posts, through: :favorites, source: :post
   has_many :comments, dependent: :destroy
+  has_many :view_couts
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(google line)
