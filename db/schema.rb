a4e862308b5ef422fc40e6de243d02eb03da04ee
genki utsumi <zabanya0770@gmail.com>
2023-07-19 19:19:32 +0900

    fix: 閲覧数のデフォルト値を0に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_19_083614) do
+ActiveRecord::Schema.define(version: 2023_07_19_100946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2023_07_19_083614) do
     t.float "latitude", null: false
     t.float "longitude", null: false
     t.text "image"
-    t.integer "view_count"
+    t.integer "view_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
