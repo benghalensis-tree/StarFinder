29783ff4f3cf09f424b3a18583a0371777aa1212
genki utsumi <zabanya0770@gmail.com>
2023-07-20 18:05:45 +0900

    add: 月齢によってカレンダーの色を変更する機能を実装
    背景色を変更したいが変わっていない状態。

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,23 @@
+require 'mk_sunmoon'
+require 'mk_calendar'
+
 module PostsHelper
+
+  def moon_age(date)
+    date_time = date.strftime('%Y%m%d')
+    day = MkCalendar.new("#{date_time}")
+    # binding.pry
+    if day.moonage > 29 || 1 > day.moonage 
+      'super_good'
+    elsif day.moonage > 25 || 5 > day.moonage 
+      'good'
+    elsif day.moonage > 20 || 10 > day.moonage 
+      'normal'
+    elsif day.moonage > 18 || 13 > day.moonage 
+      'short'
+    else
+      'bad'
+    end
+  end
+  
 end
