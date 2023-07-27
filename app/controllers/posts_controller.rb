3db95e86b821880fb7096c59a95dc7ed761e03ac
genki utsumi <zabanya0770@gmail.com>
2023-07-27 18:24:15 +0900

    add: 緯度経度情報をもとにAPIを取得できるようした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,9 @@ class PostsController < ApplicationController
     end
     @comment = @post.comments.build
     @comments = @post.comments
-    response = RestClient.get "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=weathercode&timezone=Asia%2FTokyo&forecast_days=14"
+    latitude = @post.latitude
+    longitude = @post.longitude
+    response = RestClient.get "https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&daily=weathercode&timezone=Asia%2FTokyo&forecast_days=14"
     @weather_data = JSON.parse(response.body)
   end
 
