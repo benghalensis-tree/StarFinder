38c140c6ff7f86766fac2f742148bbecee8492d0
genki utsumi <zabanya0770@gmail.com>
2023-07-28 18:10:30 +0900

    add: レーダーチャートを表示できるまで実装

---
+++ app/javascript/packs/radar_chart.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+      labels: ["空の明るさ", "透明度", "空の広さ", "アクセス", "便利さ"],
+      datasets: [{
+        label: 'ユーザーの評価',
+        backgroundColor: "rgba(0,0,80,0.4)",
+        borderColor: "rgba(0,0,80,1)",
+        data: [5,4,5,3,2]
+      }]
+    }
+  });
+  };
