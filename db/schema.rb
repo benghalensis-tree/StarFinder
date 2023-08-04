904bbd67756fca6cede4af62291ca799277a3e46
genki utsumi <zabanya0770@gmail.com>
2023-08-04 11:29:07 +0900

    add: Cityモデル,citiesテーブルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_31_073005) do
+ActiveRecord::Schema.define(version: 2023_08_04_022647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.bigint "post_id", null: false
