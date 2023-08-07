45270410515b584fbf8f7b78e7874d075079859c
genki utsumi <zabanya0770@gmail.com>
2023-08-08 08:52:52 +0900

    add: フェードインアニメーションを追加

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -69,6 +69,20 @@ window.addEventListener('DOMContentLoaded', event => {
             } 
         });
     });
+
+    $(function () {
+        $(window).scroll(function () {
+          const windowHeight = $(window).height();
+          const scroll = $(window).scrollTop();
+      
+          $('.fadein').each(function () {
+            const targetPosition = $(this).offset().top;
+            if (scroll > targetPosition - windowHeight + 100) {
+              $(this).addClass("is-fadein");
+            }
+          });
+        });
+      });
 })
 
 function fadeOut(el) {
