83b5441375fe4da02a95c9145d54b4a3eeddd4cb
genki utsumi <zabanya0770@gmail.com>
2023-08-10 22:58:31 +0900

    fix: ログインユーザーが編集できなかったので修正

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,18 +1,16 @@
 class MyPagesController < ApplicationController
+  before_action :set_user
   before_action :authenticate_user!, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts
   end
 
   def edit
-    @user = User.find(current_user.id)
   end
   
   def update
-    @user = User.find(current_user.id)
     if @user.update(user_params)
       redirect_to my_page_path(@user)
     else
@@ -21,12 +19,17 @@ class MyPagesController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:name, :content, :image, :image_cache)
   end
 
   def correct_user
-    unless current_user == @user
+    unless current_user.id == @user.id
       flash[:alert] = '権限がありません'
       redirect_to posts_path
     end
