86ee412735bb39b0dd8207b294cbb2ecb4607e83
genki utsumi <zabanya0770@gmail.com>
2023-08-10 20:53:18 +0900

    add: ログイン、ログアウト時のリダイレクト先をpostsのindexに

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,13 +5,13 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
   def new_guest_admin
     user = User.guest_admin
     sign_in user
-    redirect_to root_path, notice: '管理者ユーザーとしてログインしました。'
+    redirect_to posts_path, notice: '管理者ユーザーとしてログインしました。'
   end
 
   # GET /resource/sign_in
