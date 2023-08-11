dc44dcd27cb25af9ba036fa05ce02f5166ad08eb
genki utsumi <zabanya0770@gmail.com>
2023-08-11 13:37:16 +0900

    fix: 重複部分を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,17 +50,14 @@ class PostsController < ApplicationController
         }
       end
       @date = params[:date].to_date
-      @moon_time = moon_time(@date)
-      @day = "#{@date.month}/#{@date.day}"
-      date_time = @date.strftime('%Y%m%d')
-      @moon_age = MkCalendar.new("#{date_time}").moonage.round
+      
     else
       @date = Date.today
-      @moon_time = moon_time(@date)
-      @day = "#{@date.month}/#{@date.day}"
-      date_time = @date.strftime('%Y%m%d')
-      @moon_age = MkCalendar.new("#{date_time}").moonage.round
     end
+    @moon_time = moon_time(@date)
+    @day = "#{@date.month}/#{@date.day}"
+    date_time = @date.strftime('%Y%m%d')
+    @moon_age = MkCalendar.new("#{date_time}").moonage.round
   end
 
   def show
