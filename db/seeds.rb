69943aac9545eda055196ccb23ff3e0abcbc84f2
genki utsumi <zabanya0770@gmail.com>
2023-08-13 00:59:03 +0900

    fix: seedデータを一部修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,47 +21,48 @@ require 'gimei'
     )
   end
   
-  User.all.each do |user|
+  100.times do |i|
     Post.create!(
       title: Faker::Lorem.paragraph(sentence_count: 1),
       content: Faker::Lorem.paragraph(sentence_count: 7),
       access_date: 20230720,
       address: Gimei.address.prefecture.kanji,
       image:File.open("./public/images/image1.jpg"),
-      user_id: user.id,
+      user_id: User.all.sample.id,
       view_count: rand(3000)
     )
-
-    Post.all.each do |post|
-      Comment.create!(
-        content: Faker::Lorem.paragraph(sentence_count: 7),
-        user_id: User.all.sample.id,
-        post_id: post.id
-      )
-    end
-
-    Post.all.each do |post|
-      Rating.create!(
-        sky_light: rand(0..5),
-        sky_clear: rand(0..5),
-        sky_extent: rand(0..5),
-        accessiblity: rand(0..5),
-        convenient: rand(0..5),
-        post_id: post.id
-      )
-    end
-
-    50.times do |i|
-      ViewCount.create(
-        user_id: User.all.sample.id, 
-        post_id: Post.all.sample.id
-      )
-    end
-
-    50.times do |i|
-      Favorite.create(
-        user_id: User.all.sample.id, 
-        post_id: Post.all.sample.id
-      )
-    end
   end
+
+  500.times do |i|
+    Comment.create!(
+      content: Faker::Lorem.paragraph(sentence_count: 7),
+      user_id: User.all.sample.id,
+      post_id: Post.all.sample.id
+    )
+  end
+
+  Post.all.each do |post|
+    Rating.create!(
+      sky_light: rand(1..5),
+      sky_clear: rand(1..5),
+      sky_extent: rand(1..5),
+      accessiblity: rand(1..5),
+      convenient: rand(1..5),
+      post_id: post.id
+    )
+  end
+
+  50.times do |i|
+    ViewCount.create(
+      user_id: User.all.sample.id, 
+      post_id: Post.all.sample.id
+    )
+  end
+
+  50.times do |i|
+    Favorite.create(
+      user_id: User.all.sample.id, 
+      post_id: Post.all.sample.id
+    )
+  end
+  
