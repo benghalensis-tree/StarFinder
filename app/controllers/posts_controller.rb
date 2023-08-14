a8b8a3cc40fcf6b1ecc45e0538a458a0f62ad6af
genki utsumi <zabanya0770@gmail.com>
2023-08-14 16:48:34 +0900

    add: 削除ボタンを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -118,7 +118,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
-    redirect_to posts_url, notice: "Post was successfully destroyed."
+    redirect_to my_page_path(current_user), notice: "Post was successfully destroyed."
   end
 
   private
