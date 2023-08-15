d64b1358dab93ae8ebab1e0a1781b67ab38f09ca
genki utsumi <zabanya0770@gmail.com>
2023-08-15 15:41:19 +0900

    add: バリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   
   mount_uploader :image, ImageUploader
   validates :title, presence: true
+  validates :access_date, presence: true
   validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: lambda {|obj| obj.address_changed?}
