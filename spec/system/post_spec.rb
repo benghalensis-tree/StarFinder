bb8c66ab7f53b9ff8dd09328b9b8b63e1a732db0
genki utsumi <zabanya0770@gmail.com>
2023-08-16 15:15:44 +0900

    add: 新規投稿機能のテストを追加

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -10,4 +10,29 @@ RSpec.describe '投稿機能', type: :system do
       end
     end
   end
+  describe '新規作成機能' do
+    before do
+      @user = FactoryBot.create(:user)
+      visit new_user_session_path
+      fill_in 'user_email', with: 'user1@test.com'  
+      fill_in 'user_password', with: 111111
+      click_on 'sign_in_btn'
+      sleep 0.5
+    end
+    context '投稿を新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_post_path
+        fill_in 'post_title', with: 'テストタイトル'  
+        fill_in 'post_content', with: 'テストコンテント'  
+        find('#map').click(x: 10, y: 10)
+        attach_file 'post[image]', File.join(Rails.root, 'spec/fixtures/test.jpg')
+        choose 'sky_light4'
+        choose 'sky_clear2'
+        click_button '投稿'
+        execute_script('window.scrollBy(0,10000)')
+        expect(page).to have_content 'テストタイトル'
+        expect(page).to have_content 'テストコンテント'
+      end
+    end
+  end
 end
