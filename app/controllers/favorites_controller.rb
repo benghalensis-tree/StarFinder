86acbbe54cf48cbd82f0ab3ab278e78c1f6a2d57
genki utsumi <zabanya0770@gmail.com>
2023-08-20 01:49:51 +0900

    add: indexページでお気に入り登録ができる機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,30 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.create(post_id: params[:post_id])
     @post = Post.find(params[:post_id])
     @post.update_column(:favorite_count, @post.favorites.count)
-    redirect_to post_path(params[:post_id]), notice: "お気に入り登録しました"
+    if params[:page] == 'posts/index'
+      redirect_to posts_path, notice: "お気に入り登録しました"
+    elsif params[:page] == 'favorites/show'
+      redirect_to favorite_path(current_user), notice: "お気に入り登録しました"
+    elsif params[:page] == 'my_pages/show'
+      redirect_to my_page_path(current_user), notice: "お気に入り登録しました"
+    else
+      redirect_to post_path(params[:post_id]), notice: "お気に入り登録しました"
+    end 
   end
   
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     @post = Post.find(params[:post_id])
     @post.update_column(:favorite_count, @post.favorites.count)
-    redirect_to post_path(params[:post_id]), notice: "お気に入りを解除しました"
+    if params[:page] == 'posts/index'
+      redirect_to posts_path, notice: "お気に入りを解除しました"
+    elsif params[:page] == 'favorites/show'
+      redirect_to favorite_path(current_user), notice: "お気に入りを解除しました"
+    elsif params[:page] == 'my_pages/show'
+      redirect_to my_page_path(current_user), notice: "お気に入りを解除しました"
+    else
+      redirect_to post_path(params[:post_id]), notice: "お気に入りを解除しました"
+    end
   end
 
   def show
