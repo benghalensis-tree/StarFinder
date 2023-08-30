c5bef4501eb3cada233539e9ef31b24b06c09f91
genki utsumi <zabanya0770@gmail.com>
2023-08-31 02:52:26 +0900

    add: 詳細ページのだいたいの雛形作成

--- app/controllers/star_lights_controller.rb
+++ app/controllers/star_lights_controller.rb
@@ -1,6 +1,10 @@
 class StarLightsController < ApplicationController
 
   def show
+    @star_light = StarLight.find(params[:id])
+    lat_sec = @star_light.latitude * 3600
+    lon_sec = @star_light.longitude * 3600
+    @hotels = RakutenWebService::Travel::Hotel.search(latitude: lat_sec.round(2), longitude: lon_sec.round(2), searchRadius: 3)
   end
-  
+
 end
