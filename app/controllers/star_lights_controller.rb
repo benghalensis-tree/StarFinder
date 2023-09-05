2d006de693f9e65ea95a2b35b764c454784fe027
genki utsumi <zabanya0770@gmail.com>
2023-09-05 20:48:23 +0900

    add: 星の明るさ詳細ページのデザイン変更とレスポンシブ対応
    レスポンシブ対応
    ランクアイコン画像を削除し新たな画像を追加
    月齢カレンダー追加
    天気予報パーシャルの修正

--- app/controllers/star_lights_controller.rb
+++ app/controllers/star_lights_controller.rb
@@ -5,6 +5,10 @@ class StarLightsController < ApplicationController
     lat_sec = @star_light.latitude * 3600
     lon_sec = @star_light.longitude * 3600
     @hotels = RakutenWebService::Travel::Hotel.search(latitude: lat_sec.round(2), longitude: lon_sec.round(2), searchRadius: 3)
+    latitude = @star_light.latitude
+    longitude = @star_light.longitude
+    response = RestClient.get "https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&daily=weathercode&timezone=Asia%2FTokyo&forecast_days=7"
+    @weather_data = JSON.parse(response.body)
   end
 
 end
