e829f2bcc5852c628270892fd03cc83f2efa6964
genki utsumi <zabanya0770@gmail.com>
2023-09-08 22:12:45 +0900

    add: お気に入り機能を非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,29 +4,17 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.create(post_id: params[:post_id])
     @post = Post.find(params[:post_id])
     @post.update_column(:favorite_count, @post.favorites.count)
-    if params[:page] == 'posts/index'
-      redirect_to posts_path, notice: "お気に入り登録しました"
-    elsif params[:page] == 'favorites/show'
-      redirect_to favorite_path(current_user), notice: "お気に入り登録しました"
-    elsif params[:page] == 'my_pages/show'
-      redirect_to my_page_path(current_user), notice: "お気に入り登録しました"
-    else
-      redirect_to post_path(params[:post_id]), notice: "お気に入り登録しました"
-    end 
+    respond_to do |format|
+      format.js
+    end
   end
   
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     @post = Post.find(params[:post_id])
     @post.update_column(:favorite_count, @post.favorites.count)
-    if params[:page] == 'posts/index'
-      redirect_to posts_path, notice: "お気に入りを解除しました"
-    elsif params[:page] == 'favorites/show'
-      redirect_to favorite_path(current_user), notice: "お気に入りを解除しました"
-    elsif params[:page] == 'my_pages/show'
-      redirect_to my_page_path(current_user), notice: "お気に入りを解除しました"
-    else
-      redirect_to post_path(params[:post_id]), notice: "お気に入りを解除しました"
+    respond_to do |format|
+      format.js
     end
   end
 
