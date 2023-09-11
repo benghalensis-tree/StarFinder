404f1953560acfe24f4db05b61cd3f590332415e
genki utsumi <zabanya0770@gmail.com>
2023-09-11 18:23:38 +0900

    add: StarLightの詳細ページの天気予報もDBの情報を取得するように変更

--- app/controllers/star_lights_controller.rb
+++ app/controllers/star_lights_controller.rb
@@ -5,10 +5,13 @@ class StarLightsController < ApplicationController
     lat_sec = @star_light.latitude * 3600
     lon_sec = @star_light.longitude * 3600
     @hotels = RakutenWebService::Travel::Hotel.search(latitude: lat_sec.round(2), longitude: lon_sec.round(2), searchRadius: 3)
-    latitude = @star_light.latitude
-    longitude = @star_light.longitude
-    response = RestClient.get "https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&daily=weathercode&timezone=Asia%2FTokyo&forecast_days=7"
-    @weather_data = JSON.parse(response.body)
+    date = Date.today
+    city = City.near([@star_light.latitude, @star_light.longitude], 100).first
+    @weather_forecasts = []
+    for i in 0..7 do
+      @weather_forecasts[i] = city.weather_forecasts.find_by(date: date)
+      date += 1
+    end
   end
 
 end
